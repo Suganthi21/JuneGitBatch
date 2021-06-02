a5ba4e537671363b88d0708f3ee7388176291b43
Suganthi21 <suganthimathumitha@gmail.com>
2021-06-02 17:25:31 +0530

    Br1 added his code

--- src/org/test/Data.java
+++ src/org/test/Data.java
@@ -3,7 +3,9 @@ package org.test;
 public class Data {
 	
 	public static void main(String[] args) {
+		System.out.println("Selenium");
 		System.out.println("java");
+		System.out.println("Manual");
 	}
 
 }
