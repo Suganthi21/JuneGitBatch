257d80aeb4bd6fbabce13f1d4dba575bb147d0ef
Suganthi21 <suganthimathumitha@gmail.com>
2021-06-03 17:39:03 +0530

    Again added some lines

--- src/org/test/Data.java
+++ src/org/test/Data.java
@@ -2,11 +2,16 @@ package org.test;
 
 public class Data {
 	
+	private void print() {
+		System.out.println("Print Method ......");
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Selenium");
 		System.out.println("java");
 		System.out.println("Manual");
 		System.out.println("Python");
+		System.out.println("Testing");
 	}
 
 }
